f243e1b70db9dc0696b9e6093935c73472b5db6a
kexin76 <kexincx@gmail.com>
2024-01-30 23:05:44 -0500

    updated main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import random
 import string
 from urllib.parse import urlencode
+from hashlib import sha256
 '''
 python -m flask --app main run
 python -m flask --app main run --debug
@@ -31,7 +32,11 @@ def index():
 
 @app.route("/login")
 def login():
-    state = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+    # state = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
+    codeVerifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
+    session["codeVerifier"] = codeVerifier
+    #The line under is messed up, need to edit out certain characters out for it to always work
+    codeChallenge = base64.b64encode(sha256(codeVerifier.encode('utf-8')).digest())[:-1]
     scope = "user-read-private user-read-email user-top-read user-follow-read"
     link = 'https://accounts.spotify.com/authorize?'
     auth_url = link + urlencode({
@@ -40,14 +45,17 @@ def login():
         'redirect_uri': REDIRECT_URI,
         'scope': scope,
         # 'show_dialog': True,
-        'state': state
+        # 'state': state
+        "code_challenge_method": 'S256',
+        "code_challenge": codeChallenge
     })
     return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
+    codeVerifier = session.get("codeVerifier")
     code = request.args["code"]
-    state = request.args["state"]
+    # state = request.args["state"]
     url = 'https://accounts.spotify.com/api/token'
     headers = {
         'Authorization': 'Basic ' + AUTH_BASE64,
@@ -56,14 +64,16 @@ def callback():
     data = {
         'code': code,
         'redirect_uri': REDIRECT_URI,
-        'grant_type': 'authorization_code'
+        'grant_type': 'authorization_code',
+        "code_verifier": codeVerifier,
         }
     result = post(url, headers=headers, data=data)
     json_result = json.loads(result.content)
-    session["access_token"] = json_result["access_token"]
-    session["refresh_token"] = json_result["refresh_token"]
-    profile = getProfile()
-    return render_template('home.html', name=profile["display_name"])
+    return render_template('home.html', name=result.content)
+    # session["access_token"] = json_result["access_token"]
+    # session["refresh_token"] = json_result["refresh_token"]
+    # profile = getProfile()
+    # return render_template('home.html', name=profile["display_name"])
     
 
 @app.route('/tracks')
