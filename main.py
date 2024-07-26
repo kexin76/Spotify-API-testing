21838e5566588c5917e9d8ebeae09def13463b48
kexin76 <kexincx@gmail.com>
2024-07-26 14:26:27 -0400

    updated login and added tracks to the list

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 AUTH_BASE64 = str(base64.b64encode((str(CLIENT_ID)+":"+str(CLIENT_SECRET)).encode("utf-8")), "utf-8")
 print(CLIENT_ID,CLIENT_SECRET)
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+REDIRECT_URI = "http://127.0.0.1:5000/"
 
 
     
@@ -32,10 +32,9 @@ def index():
 
 @app.route("/login")
 def login():
-    # state = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
+    state = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
     codeVerifier = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
     session["codeVerifier"] = codeVerifier
-    #The line under is messed up, need to edit out certain characters out for it to always work
     codeChallenge = base64.b64encode(sha256(codeVerifier.encode('utf-8')).digest())[:-1]
     scope = "user-read-private user-read-email user-top-read user-follow-read"
     link = 'https://accounts.spotify.com/authorize?'
@@ -44,13 +43,14 @@ def login():
         'client_id' : CLIENT_ID,
         'redirect_uri': REDIRECT_URI,
         'scope': scope,
-        # 'show_dialog': True,
-        # 'state': state
+        'show_dialog': True,
+        'state': state,
         "code_challenge_method": 'S256',
         "code_challenge": codeChallenge
     })
     return redirect(auth_url)
 
+# used for token refresh
 @app.route('/callback')
 def callback():
     codeVerifier = session.get("codeVerifier")
@@ -68,24 +68,33 @@ def callback():
         "code_verifier": codeVerifier,
         }
     result = post(url, headers=headers, data=data)
-    if result.status_code == 200:
-        json_result = json.loads(result.content)
-        session["access_token"] = json_result["access_token"]
-        session["refresh_token"] = json_result["refresh_token"]
-        profile = getProfile()
-        return render_template('home.html', name=profile["display_name"])
     json_result = json.loads(result.content)
+    print(json_result)
     return render_template('home.html', name=result.content)
 
+@app.route('/user')
+def user():
+    profile = getProfile()
+    return redirect(profile["external_urls"]['spotify'])
+    
 @app.route('/tracks')
 def tracks():
     access_token = session.get("access_token")
     headers = get_auth_token(access_token)
-    url = 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10'
+    url = 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=20'
     result = get(url, headers=headers)
     json_result = enumerate(json.loads(result.content)["items"])
     return render_template('tracks.html', result=json_result)
 
+@app.route('/artists')
+def artists():
+    access_token = session.get("access_token")
+    headers = get_auth_token(access_token)
+    url = 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=20&offset=0'
+    result = get(url, headers=headers)
+    json_result = enumerate(json.loads(result.content)['items'])
+    return render_template('artists.html', result=json_result)
+    
 
 def get_auth_token(token): # get(url, headers=headers) where header is auth_token(token)
     auth_token = {'Authorization': 'Bearer ' + token}
